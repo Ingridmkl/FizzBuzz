0c947dedb3f100dfc0fdbd494dd9939e926955f4
vadlamudi kalpana <vadlamudikalpanaa@gmail.com>
2025-11-24 12:37:02 +0100

    Add JUnit Platform Cucumber runner `RunCucumberTest`

---
+++ src/test/java/com/example/fizzbuzz/RunCucumberTest.java
@@ -0,0 +1,8 @@
+package com.example.fizzbuzz;
+
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+public class RunCucumberTest {
+    // JUnit Platform will pick up and run Cucumber feature files.
+}
